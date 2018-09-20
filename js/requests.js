889eafcfc5202f0a2f6df3e6e85197cea9471429
Machiavellii <vjekoslav.seferovic@gmail.com>
2018-09-21 01:10:47 +0200

    Update requests.js

--- js/requests.js
+++ js/requests.js
@@ -11,7 +11,7 @@ export function getCities() {
 }
 
 export function getMeetup(name) {
-  return fetch(`https://cors-anywhere.herokuapp.com/https://api.meetup.com/2/open_events?&sign=true&photo-host=public&country=rs&city=${name}&page=20&key=80130226256d6a73312384736727c
+  return fetch(`https://cors-anywhere.herokuapp.com/https://api.meetup.com/2/open_events?&sign=true&photo-host=public&country=rs&city=${name}&page=20&key=YOURAPIKEY
   `).then(res => {
     if (res.status === 200) {
       return res.json();
